f2cf337b2252c66f77cae6a44b2441475d1524d7
mickjm <mick.mozzland@virgin.net>
2022-05-27 09:29:31 +0100

    updates

--- style/football.js
+++ style/football.js
@@ -14,18 +14,17 @@ function init() {
 function displayCorrectPageHandler() {
 
     console.log("Index : displayCorrectPageHandler");
+    var htmlString = ""
 
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
         // true for mobile device
         console.log("mobile")
-   //     document.getElementById("mobile").style.visibility = "visible";
-   //     document.getElementById("desktop").style.visibility = "hidden";
+        htmlString += '<div id="scoreaxis-widget-6b0ad-mobile"  style="border-width:1px;border-color:rgba(0, 0, 0, 0.15);border-style:solid;border-radius:8px;padding:10px;background:rgb(255, 255, 255);" data-reactroot=""><iframe id="Iframe-mobile" title="Iframe-mobile" src="https://www.scoreaxis.com/widget/standings-widget/8?widgetRows=1%2C1%2C1%2C1%2C1%2C1%2C1%2C1%2C0%2C1&amp;inst=6b0ad" style="width:100%;height:150%;border:none;transition:all 300ms ease"></iframe><script>window.addEventListener("DOMContentLoaded",event=>{window.addEventListener("message",event=>{if(event.data.appHeight&&"6b0ad"==event.data.inst){let container=document.querySelector("#scoreaxis-widget-6b0ad iframe");container&&(container.style.height=parseInt(event.data.appHeight)+"px")}},!1)});</script></div>'
 
-      }else{
+    } else{
         // false for not mobile device
         console.log("desktop")
-   //     document.getElementById("mobile").style.visibility = "hidden";
-   //     document.getElementById("desktop").style.visibility = "visible";
-      }
-
+        htmlString += '<div id="scoreaxis-widget-9b29b-desktop" style="border-width:1px;border-color:rgba(0, 0, 0, 0.15);border-style:solid;border-radius:8px;padding:10px;background:rgb(255, 255, 255);width: 100%;height:auto;" data-reactroot=""><iframe id="Iframe-desktop" title="Iframe-desktop" src="https://www.scoreaxis.com/widget/standings-widget/8?&amp;inst=9b29b" style="width:100%;height:150%;border:none;transition:all 300ms ease"></iframe><script>window.addEventListener("DOMContentLoaded",event=>{window.addEventListener("message",event=>{if(event.data.appHeight&&"9b29b"==event.data.inst){let container=document.querySelector("#scoreaxis-widget-9b29b iframe");container&&(container.style.height=parseInt(event.data.appHeight)+"px")}},!1)});</script></div>'
+    }
+    document.getElementById("leagueTable").innerHTML = htmlString;
 }
